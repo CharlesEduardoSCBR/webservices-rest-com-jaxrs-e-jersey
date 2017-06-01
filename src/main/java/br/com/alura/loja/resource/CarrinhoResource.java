3b97c928ded0ccda1d3c293088aa136a356ba377
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-05-31 23:15:19 -0300

    C1M11.05 - Alterando produtos

--- src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -41,6 +42,13 @@ public class CarrinhoResource {
 	@DELETE
 	public Response removeProduto(@PathParam("id") long id, @PathParam("produtoId") long produtoId){
 		new CarrinhoDAO().busca(id).remove(produtoId);		
+		return Response.ok().build();
+	}
+	
+	@Path("{id}/produtos/{produtoId}")
+	@PUT
+	public Response alteraProduto(@PathParam("id") long id, @PathParam("produtoId") long produtoId){
+		Carrinho carrinho = new CarrinhoDAO().busca(id);		
 		return Response.ok().build();
 	}
 }
